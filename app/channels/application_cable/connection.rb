d666d2c9c0291f315fbe625cfc97b45784a09fcd
Daniel Smolinski <Danasourasrex@Daniels-MacBook-Pro.local>
2018-04-03 13:57:16 -0400

    Chat working and Materialize SASS framework

--- app/channels/application_cable/connection.rb
+++ app/channels/application_cable/connection.rb
@@ -1,4 +1,20 @@
 module ApplicationCable
   class Connection < ActionCable::Connection::Base
+  	identified_by :current_user
+
+  	def connect
+  		self.current_user = find_verified_user
+  	end
+
+  	private
+  		def find_verified_user
+  			if current_user = User.fin_by(session[:username])
+  				current_user
+  			else
+  				reject_unauthorized_connection
+  			end
+  		end
+  	end
+
   end
 end
