eebafed818957417048acd35c9e7ce394de591e8
Michael Wojtyna <michaelwojtyna@Michaels-MacBook-Pro-2.local>
2018-04-12 14:05:43 -0400

    few updates

--- app/assets/javascripts/liveline.js
+++ app/assets/javascripts/liveline.js
@@ -7,12 +7,36 @@
 
 App.messages = App.cable.subscriptions.create('RoomChannel', {
   received: function(data) {
-  	if (data.fromx == -69.42){
+  	if (data.fromx == -1001.42){
   		canvas = $('#paper');
   		ctx = canvas[0].getContext('2d');
   		ctx.clearRect(0,0,document.getElementById('paper').width,document.getElementById('paper').height);
-  	} else {
-    drawLine(data.fromx, data.fromy, data.tox, data.toy, data.color)
+  	}else if (data.fromx == -1000.42){
+  		
+  		
+  		var timeLeft = 10;
+  		var secondslabel = "";
+  		 clearInterval(timer);
+
+		timer = setInterval(function(){
+		document.getElementById("time_left").innerHTML = "Time left:";
+		var secondslabel = document.getElementById("timer").value = 0 + --timeLeft;
+		document.getElementById("timer").innerHTML = secondslabel;
+		
+		if(timeLeft <= 0)
+		{
+  			clearInterval(timer);
+  			document.getElementById("time_left").innerHTML = "Time is up!";
+  			document.getElementById("timer").innerHTML = "";
+		}
+
+		}, 1000);
+
+  	}
+  	 else{
+  	
+    	drawLine(data.fromx, data.fromy, data.tox, data.toy, data.color)
+	}
 }
   }
-});
+);
