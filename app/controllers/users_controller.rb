9ec6243ec4e28c5f95d243c3ea5d5e7160adb9df
Dan S <Danasourasrex@Daniels-MacBook-Pro.local>
2018-04-29 18:46:48 -0400

    Added duplicate user creation checking. Added Admin link if logged in as admin. Cleaned some routes.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -25,15 +25,20 @@ class UsersController < ApplicationController
   # POST /users
   # POST /users.json
   def create
-    @user = User.new(user_params)
+    p user_params
+    if @existing = User.find_by(email: user_params[:email])
+        redirect_to "/signup"
+    else
+      @user = User.new(user_params)
 
-    respond_to do |format|
-      if @user.save
-        format.html { redirect_to @user, notice: 'User was successfully created.' }
-        format.json { render :show, status: :created, location: @user }
-      else
-        format.html { render :new }
-        format.json { render json: @user.errors, status: :unprocessable_entity }
+      respond_to do |format| 
+        if @user.save
+          format.html { redirect_to @user, notice: 'User was successfully created.' }
+          format.json { render :show, status: :created, location: @user }
+        else
+          format.html { render :new }
+          format.json { render json: @user.errors, status: :unprocessable_entity }
+        end
       end
     end
   end
